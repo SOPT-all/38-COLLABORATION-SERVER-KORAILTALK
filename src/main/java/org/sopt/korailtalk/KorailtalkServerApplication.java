de88cd1fe563f5db347206426957cdc3d40b03d5
NohSungBeen <castlekong1019@gmail.com>
2026-05-09 03:22:56 +0900

    chore: Spring Boot 기본 세팅 및 docker-compose 세팅

---
+++ src/main/java/org/sopt/korailtalk/KorailtalkServerApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.korailtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorailtalkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KorailtalkServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sopt/korailtalk/KorailtalkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.korailtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorailtalkServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
