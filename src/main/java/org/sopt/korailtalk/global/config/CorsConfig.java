b1ecd8a967741261d7ef5dbd4894b9c02cc36dd3
NohSungBeen <castlekong1019@gmail.com>
2026-05-09 04:45:37 +0900

    chore: CORS 초기 설정

---
+++ src/main/java/org/sopt/korailtalk/global/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package org.sopt.korailtalk.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:5173",
+                        "http://localhost:8080" //,
+                        //"https:// 추가 할도메인"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
