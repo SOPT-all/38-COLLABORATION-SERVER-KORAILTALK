4a43aa51c3f8755f428db7a3fd2550043d430891
khj011219 <khj011219@naver.com>
2026-05-14 17:03:44 +0900

    feat: ReservationSeat 엔티티 구현

---
+++ src/main/java/org/sopt/korailtalk/reservation/domain/ReservationSeat.java
@@ -0,0 +1,45 @@
+package org.sopt.korailtalk.reservation.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.korailtalk.schedule.domain.Schedule;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "reservation_seats")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReservationSeat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id", nullable = false)
+    private Schedule schedule;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "seat_number", nullable = false, length = 20)
+    private String seatNumber;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public ReservationSeat(
+            Schedule schedule,
+            Long userId,
+            String seatNumber
+    ) {
+        this.schedule = schedule;
+        this.userId = userId;
+        this.seatNumber = seatNumber;
+    }
+}
