d20fe143e122848821bcb88edec29dc0e2f10f20
NohSungBeen <castlekong1019@gmail.com>
2026-05-15 18:36:50 +0900

    fix: 원격 Swagger CORS 허용

--- src/main/java/org/sopt/korailtalk/global/config/CorsConfig.java
+++ src/main/java/org/sopt/korailtalk/global/config/CorsConfig.java
@@ -13,8 +13,8 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:3000",
                         "http://localhost:5173",
-                        "http://localhost:8080" //,
-                        //"https:// 추가 할도메인"
+                        "http://localhost:8080",
+                        "https://38-sopt-korailtalk.p-e.kr"
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
